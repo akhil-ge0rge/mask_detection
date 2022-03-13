8d5baeecfbb7ee6c3b2ec164b458e9961e2af7e9
Akhil George <akhil8311@gmail.com>
2022-03-13 12:16:31 +0530

    Project completed

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +13,64 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //============================================================================
+  bool _loading = true;
+  late File _image;
+  final imagepicker = ImagePicker();
+  List _predictions = [];
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+//============================================================================
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  detect_image(File image) async {
+    var prediction = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+
+    setState(() {
+      _loading = false;
+      _predictions = prediction!;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  _loadimage_gallery() async {
+    var image = await imagepicker.pickImage(source: ImageSource.gallery);
+    if (Image == null) {
+      return null;
+    } else {
+      _image = File(image!.path);
+    }
+    detect_image(_image);
+  }
+
+  _loadimage_camera() async {
+    var image = await imagepicker.pickImage(source: ImageSource.camera);
+    if (Image == null) {
+      return null;
+    } else {
+      _image = File(image!.path);
+    }
+    detect_image(_image);
+  }
+
+  //============================================================================
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -47,7 +109,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 Icons.camera,
                 color: Color.fromARGB(255, 37, 37, 37),
               ),
-              onPressed: () {},
+              onPressed: () {
+                _loadimage_camera();
+              },
               label: const Text(
                 "Capture",
                 style: TextStyle(
@@ -70,7 +134,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 Icons.upload_file,
                 color: Color.fromARGB(255, 37, 37, 37),
               ),
-              onPressed: () {},
+              onPressed: () {
+                _loadimage_gallery();
+              },
               label: const Text(
                 "Upload",
                 style: TextStyle(
@@ -85,6 +151,33 @@ class _HomeScreenState extends State<HomeScreen> {
                 textStyle: const TextStyle(),
               ),
             ),
+            SizedBox(
+              height: 25,
+            ),
+            _loading == false
+                ? Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 200,
+                          width: 180,
+                          child: Image.file(_image),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          _predictions[0]['label']
+                              .toString()
+                              .substring(2)
+                              .toUpperCase(),
+                          style:
+                              GoogleFonts.roboto(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
           ],
         ),
       ),
